d9dd7b1e9293197cd7a6c01a1410a62510ae5ae6
Kaleb Juliu <kalebjuliu@gmail.com>
2018-07-09 10:15:20 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+document.getElementById('button').addEventListener('click', getJokes)
+
+function getJokes(e){
+
+  const xhr = new XMLHttpRequest()
+
+  xhr.open('GET', 'http://api.icndb.com/jokes/random', true)
+
+
+  xhr.onload = function(){
+    const response = JSON.parse(this.responseText)
+    const joke = response.value.joke
+
+    document.getElementById('joke').innerHTML = joke
+  }
+
+  xhr.send()
+
+  e.preventDefault()
+}
